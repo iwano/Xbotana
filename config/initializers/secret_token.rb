c3de0e53d5239a3e3d7d6b6b200fb7fab53a3d32
Ivan Calderon <iwano@hotmail.com>
2012-04-10 22:10:06 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xbotana::Application.config.secret_token = '1c383a6e907aba26a2e0c53d55ce0fa5add20d3094e9e59bb65dd3ccb109425db0f6778b5f631519c3255b1b0ef26f602b546c0ee5dcc525b6431ebaf9b08ff8'
